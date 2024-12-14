3666b375b0a0bd6bf04ac27c89bec7c9f35effa6
M-ahmad144 <muhammadahmadmughal0987@gmail.com>
2024-12-15 00:17:14 +0500

    error

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -6,10 +6,6 @@ const ErrorHandler = require("../utils/errorHandler");
 exports.authMiddleware = asyncHandler(async (req, res, next) => {
   const token = req.cookies.jwt;
 
-  if (!token) {
-    return next(new ErrorHandler("No token, authorization denied", 401));
-  }
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.userId);
