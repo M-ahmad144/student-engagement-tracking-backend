da4784ac6db8992eba527f193fee104b4f2a0b4c
M-ahmad144 <muhammadahmadmughal0987@gmail.com>
2024-12-15 21:45:09 +0500

    token issue

--- utils/token.js
+++ utils/token.js
@@ -2,15 +2,15 @@ const jwt = require("jsonwebtoken");
 
 const generateToken = (res, userId) => {
   // Generate the JWT token
-  const token = jwt.sign({ userId }, "my-secret-key", {
-    expiresIn: "30d",
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "30d", // Token validity
   });
 
   // Set the JWT token in an HTTP-only cookie
   res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "strict",
+    httpOnly: true, // Protects against XSS
+    secure: process.env.NODE_ENV === "production", // Ensures secure cookies in production
+    sameSite: "strict", // Prevents CSRF
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
   });
 
