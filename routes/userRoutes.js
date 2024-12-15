753c4b2654ddabbe422a4ff47c3b4afd69439518
M-ahmad144 <muhammadahmadmughal0987@gmail.com>
2024-12-15 21:09:49 +0500

    error fix

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -20,12 +20,13 @@ router.post("/register", signupUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutCurrentUser);
 
+router.patch("/profile", updateCurrentUserProfile);
+
 // Protected routes
 router.use(authMiddleware);
 
 // Profile routes (user-protected routes)
 router.get("/profile", getCurrentUserProfile);
-router.patch("/profile", updateCurrentUserProfile);
 
 // Admin routes
 router.get("/", authAdmin, getAllUsers);
