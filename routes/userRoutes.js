0bfa8b92e15474179a7a34b7d5df255352dfd0a1
M-ahmad144 <muhammadahmadmughal0987@gmail.com>
2024-12-17 20:21:58 +0500

    deploying ...

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -25,7 +25,7 @@ router.use(authMiddleware);
 
 // Profile routes (user-protected routes)
 router.get("/profile", getCurrentUserProfile);
-router.patch("/profile", updateCurrentUserProfile);
+router.patch("/profile", updateCurrentUserProfile); // Make sure the path matches frontend
 
 // Admin routes
 router.get("/", authAdmin, getAllUsers);
